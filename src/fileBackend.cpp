319f0772b38e1f59d85f4b9e80b461deed7c004f
Richard Raj <mrichardraj@gmail.com>
2019-02-14 20:13:54 -0500

    Fix Indentation

--- src/fileBackend.cpp
+++ src/fileBackend.cpp
@@ -3,10 +3,10 @@
 // Licensed under the MIT license. See LICENSE file for details.
 //
 
-#include<iostream>
+#include <iostream>
 #include "cppdb/fileBackend.h"
 
-cppdb::Status cppdb::FileBackend::open(const std::string& name) {
+cppdb::Status cppdb::FileBackend::open(const std::string &name) {
   // Creates a new file / opens an exisiting file in append mode
   // Returns true/false status of the opertaion which will be converted to a
   // proper Status instance by the DB wrapper
@@ -14,9 +14,9 @@ cppdb::Status cppdb::FileBackend::open(const std::string& name) {
     return cppdb::Status::Error();
   }
   if (f.is_open()) {
-      f.close();
+    f.close();
   }
-  f.open(name,  std::fstream::app);
+  f.open(name, std::fstream::app);
   // f.close();
   if (isOk()) {
     return cppdb::Status::OK();
@@ -32,19 +32,19 @@ cppdb::Status cppdb::FileBackend::close() {
   return cppdb::Status::Error();
 }
 
-cppdb::Status cppdb::FileBackend::put(const std::string& key,
-    const std::string& value) {
-    if (key.empty() || value.empty()) {
-        return cppdb::Status::Error();
-    }
-    if (!isOk()) {
-        return cppdb::Status::Error();
-    }
-    f << key << "," << value << std::endl;
-    if (isOk()) {
-      return cppdb::Status::OK();
-    }
+cppdb::Status cppdb::FileBackend::put(const std::string &key,
+                                      const std::string &value) {
+  if (key.empty() || value.empty()) {
     return cppdb::Status::Error();
+  }
+  if (!isOk()) {
+    return cppdb::Status::Error();
+  }
+  f << key << "," << value << std::endl;
+  if (isOk()) {
+    return cppdb::Status::OK();
+  }
+  return cppdb::Status::Error();
 }
 
 bool cppdb::FileBackend::isOk() {
